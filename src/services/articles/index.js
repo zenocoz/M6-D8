2c32ec0f6330af10dc791ba20aad1b9375547a05
zenocoz <federico.soncini@gmail.com>
2021-01-19 17:51:58 +0100

    delete and get by id

--- src/services/articles/index.js
+++ src/services/articles/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 
-const db = require("../../utils/db")
+// const db = require("../../utils/db")
 const Model = require("../../utils/model")
 const Articles = new Model("articles")
 
@@ -14,17 +14,15 @@ router.get("/", async (req, res, next) => {
   }
 })
 
-// router.get("/:id", async (req, res, next) => {
-//   try {
-//     const { rows } = await db.query(
-//       `SELECT * FROM tutors WHERE id=${parseInt(req.params.id, 10)}`
-//     )
-//     res.send(rows)
-//   } catch (e) {
-//     console.log(e)
-//     res.status(500).send(e)
-//   }
-// })
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { rows } = await Articles.findById(req.params.id)
+    res.send(rows)
+  } catch (e) {
+    console.log(e)
+    res.status(500).send(e)
+  }
+})
 
 router.post("/", async (req, res, next) => {
   try {
@@ -45,16 +43,14 @@ router.put("/:id", async (req, res, next) => {
   }
 })
 
-// router.delete("/:id", async (req, res, next) => {
-//   try {
-//     const { rows } = await db.query(
-//       `DELETE FROM tutors WHERE id=${parseInt(req.params.id, 10)}`
-//     )
-//     res.send(rows)
-//   } catch (e) {
-//     console.log(e)
-//     res.status(500).send(e)
-//   }
-// })
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { rows } = await Articles.findByIdAndDelete(req.params.id)
+    res.send(rows)
+  } catch (e) {
+    console.log(e)
+    res.status(500).send(e)
+  }
+})
 
 module.exports = router
